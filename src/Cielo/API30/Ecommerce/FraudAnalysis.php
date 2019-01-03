88a65c499dccaf8d1ba4319038f17ebae7757ab0
Eduardo Soares <eduardomatsuzaki@gmail.com>
2019-01-03 01:39:28 -0200

    Update FraudAnalysis.php

--- src/Cielo/API30/Ecommerce/FraudAnalysis.php
+++ src/Cielo/API30/Ecommerce/FraudAnalysis.php
@@ -14,21 +14,26 @@ class FraudAnalysis implements \JsonSerializable
 
     private $fingerPrintId;
 
+    private $totalOrderAmount;
+
     private $browser;
 
     private $cart;
 
-    private $merchantDefinedFields;
+    public $replyData;
 
-    private $shipping;
+    //private $merchantDefinedFields;
 
-    private $travel;
+    //private $shipping;
 
+    //private $travel;
 
-    public function __construct()
+    public function __construct($totalOrderAmount = null, $fingerPrintId = null)
     {
         $this->setSequence('AuthorizeFirst');
         $this->setSequenceCriteria('OnSuccess');
+        $this->setTotalOrderAmount($totalOrderAmount);
+        $this->setFingerPrintId($fingerPrintId);
     }
 
     public function populate(\stdClass $data)
@@ -36,12 +41,12 @@ class FraudAnalysis implements \JsonSerializable
         $dataProps = get_object_vars($data);
 
         if (isset($dataProps['Browser'])) {
-            $this->browser = new Browser();
+            $this->browser = new Browser($data->Browser);
             $this->browser->populate($data->Browser);
         }
 
         if (isset($dataProps['Cart'])) {
-            $this->cart = new Cart();
+            $this->cart = new Cart($data->Cart);
             $this->cart->populate($data->Cart);
         }
 
@@ -59,6 +64,14 @@ class FraudAnalysis implements \JsonSerializable
             $this->travel = new Travel();
             $this->travel->populate($data->Travel);
         }
+
+        if (isset($dataProps['FingerPrintId'])) {
+            $this->fingerPrintId = $dataProps['FingerPrintId'];
+        }
+
+        if (isset($dataProps['ReplyData'])) {
+            $this->replyData = $dataProps['ReplyData'];
+        }
     }
 
     public function jsonSerialize()
@@ -66,51 +79,51 @@ class FraudAnalysis implements \JsonSerializable
         return get_object_vars($this);
     }
 
-//    public function browser($type, $ip)
-//    {
-//        $newBrowser = new Browser($type, $ip);
-//
-//        $this->setBrowser($newBrowser);
-//
-//        return $newBrowser;
-//    }
-//
-//    public function cart($isGift, $returnsAccepted, $items)
-//    {
-//        $newCart = new Cart($isGift, $returnsAccepted, $items);
-//
-//        $this->setCart($newCart);
-//
-//        return $newCart;
-//    }
-//
-//    public function merchantDefinedFields()
-//    {
-//        $newMerchantDefinedFields = new MerchantDefinedFields();
-//
-//        $this->setMerchantDefinedFields($newMerchantDefinedFields);
-//
-//        return $newMerchantDefinedFields;
-//    }
-//
-//    public function shipping($addressee, $method, $phone)
-//    {
-//        $newShippingBilling = new ShippingBilling($addressee, $method, $phone);
-//
-//        $this->setShippingBilling($newShippingBilling);
-//
-//        return $newShippingBilling;
-//    }
-//
-//    public function travel($departureTime, $journeyType, $route, $legs)
-//    {
-//        $newTravel = new Travel($departureTime, $journeyType, $route, $legs);
-//
-//        $this->setTravel($newTravel);
-//
-//        return $newTravel;
-//    }
+    public function browser($browserData)
+    {
+        $newBrowser = new Browser($browserData);
 
+        $this->setBrowser($newBrowser);
+
+        return $newBrowser;
+    }
+
+    public function cart($cartData)
+    {
+        $newCart = new Cart($cartData);
+
+        $this->setCart($newCart);
+
+        return $newCart;
+    }
+
+    public function merchantDefinedFields()
+    {
+        $newMerchantDefinedFields = new MerchantDefinedFields();
+
+        $this->setMerchantDefinedFields($newMerchantDefinedFields);
+
+        return $newMerchantDefinedFields;
+    }
+
+    public function shipping($addressee, $method, $phone)
+    {
+        $newShippingBilling = new ShippingBilling($addressee, $method, $phone);
+
+        $this->setShippingBilling($newShippingBilling);
+
+        return $newShippingBilling;
+    }
+
+/*    public function travel($departureTime, $journeyType, $route, $legs)
+    {
+        $newTravel = new Travel($departureTime, $journeyType, $route, $legs);
+
+        $this->setTravel($newTravel);
+
+        return $newTravel;
+    }
+*/
     public function setSequence($sequence)
     {
         $this->sequence = $sequence;
@@ -128,6 +141,28 @@ class FraudAnalysis implements \JsonSerializable
     public function setFingerPrintId($fingerPrintId)
     {
         $this->fingerPrintId = $fingerPrintId;
+
+        return $this;
+    }
+
+
+    public function setTotalOrderAmount($totalOrderAmount)
+    {
+        $this->totalOrderAmount = $totalOrderAmount;
+
+        return $this;
+    }
+
+    public function setCart($cart)
+    {
+        $this->cart = $cart;
+
+        return $this;
+    }
+
+    public function setBrowser($browser)
+    {
+        $this->browser = $browser;
 
         return $this;
     }
